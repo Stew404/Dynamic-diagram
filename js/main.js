9d7b3b72dd6120d49511db0adc67b717e2b6edc3
Stew404 <artembotov@mail.ru>
2020-12-12 20:06:16 +0300

    added promoters feature

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const percentInput = document.querySelector('.get-percent');
       percentInputBtn = document.querySelector('.get-percent-btn');
-      percent = document.querySelector('.percent');
+      percentCls = document.querySelector('.percent');
       percentFloat = document.querySelector('.float');
       greenCircle = document.querySelector('#green-circle');
       yellowCircle = document.querySelector('#yellow-circle');
@@ -12,29 +12,163 @@ const percentInput = document.querySelector('.get-percent');
       fortniteVideo = document.querySelector('.video-fortnite');
       fortniteGradient = document.querySelector('.fortnite-gradient');
       flagImg = document.querySelector('.flag-img');
+      statsBlockPercentValue = document.querySelector('.stats-block-percent-value');
+      statsBlockNewPercent = document.querySelector('.new-percent');
+      statsBlockPrevPercent = document.querySelector('.prev-percent');
+      rectStyle = document.querySelector('.rect-style');
+      statsBlockVisualDifference = document.querySelector('.stats-block-visual-difference');
+
 
 var circleLength = 604;
     startPositiong = circleLength;
     startPositiony = circleLength;
 
+
+
+// percentInputBtn.addEventListener('click', () => {
+//       percent.innerHTML = valueInt + "<span class=\"float\">" + valueFloat + "</span>";
+
+//       circleStyle.innerHTML=`
+//       .cls-1, .cls-2, .cls-3 {
+//                   fill:none;
+//                   stroke-linecap:round;
+//                   stroke-dasharray: 604;
+//                   stroke-dashoffset: 0;
+//                }
+//                .cls-1 {
+//                   stroke:#ececec;
+//                   stroke-linejoin:round;
+//                   stroke-width:7px;
+//                   animation: drawGrey 1s linear;
+//                }
+//                .cls-2 {
+                  
+//                   stroke:#693;
+//                   stroke-width:8px;
+                  
+//                }
+//                .cls-2, .cls-3 {
+//                   stroke-miterlimit:10;
+//                   stroke-dashoffset: 604;
+//                }
+//                .cls-3 {
+                  
+//                   stroke:#fcd329;
+//                   stroke-width:9px;
+//                }
+
+//                .active-green {
+//                   animation: drawGreen 1s linear;
+//                }
+
+//                .active-yellow {
+//                   animation: drawYellow 1s linear;
+//                }
+//       `;
+      
+//       setTimeout(updateCircles, 0, percentInputValue*0.01);
+      
+//    } 
+//    else {
+//      alert('Вводить можно только числа')
+//    }
+
+   
+
+// })
+
+    
+    var prevPercent = 0;
+      greenPercent ="";
+      valueInt ="";
+      valueFloat ="";
+      mainPercent = 0;
+
+percentInputBtn.addEventListener('click', () => {
+   var percentInputValue = parseFloat(percentInput.value);
+
+   greenRect(percentInputValue);
+
+   updateCircles(greenPercent);
+});
+
+function greenRect(percentInputValue) {
+   var newPercent = percentInputValue;
+      greenPercent = percentInputValue*0.01;
+      percentDifference = newPercent - prevPercent;
+
+   valuesArr = FloatSlicer(percentInputValue); 
+
+   statsBlockPercentValue.innerHTML = valuesArr[0] + "." + valuesArr[1] + "%";
+
+   if (percentDifference > 0){
+      statsBlockVisualDifference.innerHTML = "+" + percentDifference + "%";
+   } 
+   else {
+      statsBlockVisualDifference.innerHTML = percentDifference + "%"
+   }
+   
+   rectStyle.innerHTML= `
+   .new-percent {
+      width: `+ newPercent +`%;
+      animation: DrawGreenRect 1s;
+   }
+   .prev-percent {
+      width: `+ prevPercent +`%;
+      animation: DrawGreenSubRect 1s;
+   }
+
+   @keyframes DrawGreenRect {
+      0% {
+         width: 0;
+      }
+
+      100% {
+         width: `+ newPercent +`%;
+      }
+   }
+
+   @keyframes DrawGreenSubRect {
+      0% {
+         width: 0;
+      }
+
+      100% {
+         width: `+ prevPercent +`%;
+      }
+   }
+   `;
+   prevPercent = newPercent;
+}
+
+function gloatSlicer(inputValue) {
+   var valueInt = parseInt(inputValue);
+      valueFloat = inputValue - valueInt;
+      valueFloat = valueFloat.toFixed(1);
+      valueFloat *= 10;
+
+
+      if (valueFloat == 10) {
+         valueFloat = 9;
+      }
+      let arr = [valueInt, valueFloat];
+   return arr;
+}
+
 function updateCircles(percent) {
 
-       var percentLast = percent;
+       valuesArr = FloatSlicer(percent*100); 
+       percentCls.innerHTML = valuesArr[0] + "<span class=\"float\">" + valuesArr[1] + "</span>";
 
-       animationDuration = percent.toFixed(1);
+       var percentLast = percent;
+         animationDuration = percent.toFixed(1);
 
        if (Math.abs(percentLast-percent) < 0.4) {
           animationDuration = 0.4;
        }
 
-
-
          endPositiong = circleLength - (circleLength * percent);
-         endPositiony = circleLength - (circleLength * percent)/2;
-
-
-       
-
+         endPositiony = 604;
 
        circleStyle.innerHTML = `
                .cls-1, .cls-2, .cls-3 {
@@ -102,92 +236,10 @@ function updateCircles(percent) {
                   }
                }
          `;
-
            startPositiong = endPositiong;
            startPositiony = endPositiony;
-
-
 }
 
-percentInputBtn.addEventListener('click', () => {
-   var percentInputValue = parseFloat(percentInput.value);
-
-   if (percentInputValue < 0 || percentInputValue > 100) {
-      alert('ТЫ ЕБЛАН? Проценты бывают только от 0 до 100. ДАУН');
-   }
-   else if (percentInputValue == 69) {
-      fortniteWrapper.classList.add("video-active");
-      fortniteGradient.classList.add("gradient-active");
-      flagImg.classList.add("opacity-none");
-      fortniteVideo.play();
-      fortniteVideo.addEventListener('ended', () =>{
-         fortniteWrapper.classList.remove("video-active");
-         fortniteGradient.classList.remove("gradient-active");
-         flagImg.classList.remove("opacity-none");
-      })
-   }
-   else if (!isNaN(percentInputValue)) {
-      var valueInt = parseInt(percentInputValue);
-          valueFloat = percentInputValue - valueInt;
-          
-      valueFloat = valueFloat.toFixed(1);
-      valueFloat *= 10;
-
-      if (valueFloat == 10) {
-         valueFloat = 9;
-      }
-
-      percent.innerHTML = valueInt + "<span class=\"float\">" + valueFloat + "</span>";
-
-      circleStyle.innerHTML=`
-      .cls-1, .cls-2, .cls-3 {
-                  fill:none;
-                  stroke-linecap:round;
-                  stroke-dasharray: 604;
-                  stroke-dashoffset: 0;
-               }
-               .cls-1 {
-                  stroke:#ececec;
-                  stroke-linejoin:round;
-                  stroke-width:7px;
-                  animation: drawGrey 1s linear;
-               }
-               .cls-2 {
-                  
-                  stroke:#693;
-                  stroke-width:8px;
-                  
-               }
-               .cls-2, .cls-3 {
-                  stroke-miterlimit:10;
-                  stroke-dashoffset: 604;
-               }
-               .cls-3 {
-                  
-                  stroke:#fcd329;
-                  stroke-width:9px;
-               }
-
-               .active-green {
-                  animation: drawGreen 1s linear;
-               }
-
-               .active-yellow {
-                  animation: drawYellow 1s linear;
-               }
-      `;
-      
-      setTimeout(updateCircles, 0, percentInputValue*0.01);
-      
-   } 
-   else {
-      gorinWrapper.classList.add("video-active");
-      gorinVideo.play();
-      gorinVideo.addEventListener('ended', () =>{
-         gorinWrapper.classList.remove("video-active");
-      })
-   }
 
 
-})
 
